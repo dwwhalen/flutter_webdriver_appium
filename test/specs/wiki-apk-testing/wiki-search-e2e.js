25177ac687a7386da92a79a33083a630e6924810
Dennis Whalen <dwwhalen@gmail.com>
2023-04-25 12:23:18 -0400

    renaming some files

---
+++ test/specs/wiki-apk-testing/wiki-search-e2e.js
@@ -0,0 +1,18 @@
+const WikiPage = require('../../pageobjects/wiki-page');
+var assert = require('assert');
+
+describe('Wiki page', () => {
+    it('search should return results', async () => {
+
+        await WikiPage.clickSearchTextbox();
+
+        await WikiPage.insertTextAndWait();
+
+        var searchResults = await WikiPage.searchResults;
+
+        await assert(searchResults.length > 0);
+
+    });
+});
+
+

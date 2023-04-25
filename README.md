43aca8ea946ff70b01d22077fbdcaad35b0ee5c2
Dennis Whalen <dwwhalen@gmail.com>
2023-04-25 18:05:44 -0400

    got the flutter test running locally for both ios and android

--- README.md
+++ README.md
@@ -8,19 +8,27 @@ This repo will eventually provide an example E2E testing of a Flutter mobile app
 `npm install`
 
 
-
-## Run a test locally 
-### start appium
-`appium`
-### run the test
-`npm run test`
-
 ## Running in BrowserStack
 ### set your BrowserStack environment variables
 ```
 export BROWSERSTACK_USERNAME={your username}
 export BROWSERSTACK_ACCESS_KEY={your access key}
 ```
+
+## Run a test 
+### start appium
+`appium`
+### run the wikipedia android test locally
+`npm run test-wiki`
+### run the wikipedia android test locally
+`npm run test-wiki-bs`
+
+### run the flutter android test locally
+`npm run test-hello-android`
+### run the flutter ios test locally
+`npm run test-hello-ios`
+
+
 
 ### Run a test in BrowserStack
 `npm run test-bs`
---
+++ test/specs/hello-testing/hello-test.js
@@ -0,0 +1,28 @@
+const WikiPage = require('../../pageobjects/wiki-page');
+var assert = require('assert');
+const find = require('appium-flutter-finder');
+
+describe('Hello World app', () => {
+    it('Clicking the button should increment the counter', async () => {
+
+        // await browser.pause(5000);
+        const counterTextFinder = find.byValueKey('counter');
+        const buttonFinder = find.byValueKey('increment');
+
+        assert.strictEqual(await browser.getElementText(counterTextFinder), '0');
+
+        await browser.elementClick(buttonFinder);
+        assert.strictEqual(await driver.getElementText(counterTextFinder), '1');
+
+        await browser.touchAction({
+            action: 'tap',
+            element: { elementId: buttonFinder }
+        });
+        assert.strictEqual(await driver.getElementText(counterTextFinder), '2');
+
+        await driver.elementClick(find.byTooltip('Increment'));
+        assert.strictEqual(await driver.getElementText(counterTextFinder), '3');
+    });
+});
+
+
